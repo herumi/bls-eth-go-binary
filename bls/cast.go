5694c329cf057f59230e7747fc8605b21ae5a1df
MITSUNARI Shigeo <herumi@nifty.com>
2019-10-27 12:01:24 +0900

    remove Copy{From,To} functions

--- bls/cast.go
+++ bls/cast.go
@@ -14,14 +14,6 @@ func CastToSecretKey(in *Fr) *SecretKey {
 	return (*SecretKey)(unsafe.Pointer(in))
 }
 
-func CopyFromSecretKey(in *SecretKey) Fr {
-	return *CastFromSecretKey(in)
-}
-
-func CopyToSecretKey(in *Fr) SecretKey {
-	return *CastToSecretKey(in)
-}
-
 // PublicKey
 
 func CastFromPublicKey(in *PublicKey) *G1 {
@@ -32,14 +24,6 @@ func CastToPublicKey(in *G1) *PublicKey {
 	return (*PublicKey)(unsafe.Pointer(in))
 }
 
-func CopyFromPublicKey(in *PublicKey) G1 {
-	return *CastFromPublicKey(in)
-}
-
-func CopyToPublicKey(in *G1) PublicKey {
-	return *CastToPublicKey(in)
-}
-
 // Sign
 
 func CastFromSign(in *Sign) *G2 {
@@ -48,12 +32,4 @@ func CastFromSign(in *Sign) *G2 {
 
 func CastToSign(in *G2) *Sign {
 	return (*Sign)(unsafe.Pointer(in))
-}
-
-func CopyFromSign(in *Sign) G2 {
-	return *CastFromSign(in)
-}
-
-func CopyToSign(in *G2) Sign {
-	return *CastToSign(in)
 }
