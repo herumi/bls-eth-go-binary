3aab6e98aeb27b4022413325631372a205b16efa
MITSUNARI Shigeo <herumi@nifty.com>
2020-03-25 11:10:00 +0900

    add testVerifyHashWithDomain

--- bls/bls_test.go
+++ bls/bls_test.go
@@ -349,6 +349,34 @@ func testEth(t *testing.T) {
 	blsAggregateVerifyNoCheckTest(t)
 }
 
+func testVerifyHashWithDomain(t *testing.T) {
+	const S = 40
+	const N = 10
+	secs := make([]SecretKey, N)
+	pubs := make([]PublicKey, N)
+	sigs := make([]Sign, N)
+	hds := make([]byte, S*N)
+	for i := 0; i < N; i++ {
+		hds[i*S] = byte(i)
+		hd := hds[S*i : S*(i+1)]
+		secs[i].SetByCSPRNG()
+		pubs[i] = *secs[i].GetPublicKey()
+		sigs[i] = *secs[i].SignHashWithDomain(hd)
+		if !sigs[i].VerifyHashWithDomain(&pubs[i], hd) {
+			t.Fatalf("bad VerifyHashWithDomain %v", i)
+		}
+	}
+	var sig Sign
+	sig.Aggregate(sigs)
+	if !sig.VerifyAggregateHashWithDomain(pubs, hds) {
+		t.Fatalf("bad VerifyAggregateHashWithDomain")
+	}
+	hds[0] = 5
+	if sig.VerifyAggregateHashWithDomain(pubs, hds) {
+		t.Fatalf("bad VerifyAggregateHashWithDomain 2")
+	}
+}
+
 func Test(t *testing.T) {
 	if Init(BLS12_381) != nil {
 		t.Fatalf("Init")
@@ -368,6 +396,7 @@ func Test(t *testing.T) {
 	testCompressedG2(t, &gen2)
 	testSignAndVerifyHash(t)
 	testVerifyAggreageteHash(t)
+	testVerifyHashWithDomain(t)
 	testEth(t)
 }
 
