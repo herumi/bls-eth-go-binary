3a76b4c6c5998a3eef10f5a12a06f25450ba1764
MITSUNARI Shigeo <herumi@nifty.com>
2020-06-05 17:20:07 +0900

    add sample of EthModeDraft07

--- examples/sample.go
+++ examples/sample.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"github.com/herumi/bls-eth-go-binary/bls"
 )
 
 func sample1() {
+	fmt.Printf("sample1\n")
+	var sec bls.SecretKey
+	sec.SetByCSPRNG()
+	msg := []byte("abc")
+	pub := sec.GetPublicKey()
+	sig := sec.SignByte(msg)
+	fmt.Printf("verify=%v\n", sig.VerifyByte(pub, msg))
+}
+
+func sample2() {
+	fmt.Printf("sample2\n")
 	var sec bls.SecretKey
 	sec.SetByCSPRNG()
 	fmt.Printf("sec:%s\n", sec.SerializeToHexStr())
@@ -21,20 +33,8 @@ func sample1() {
 	fmt.Printf("P.Z=%x\n", P.Z.Serialize())
 }
 
-func sample2() {
-	var sec bls.SecretKey
-	sec.DeserializeHexStr("47b8192d77bf871b62e87859d653922725724a5c031afeabc60bcef5ff665138")
-
-	msg := [40]byte{1, 2, 3}
-	fmt.Printf("sec=%v\n", sec.Serialize())
-	pub := sec.GetPublicKey()
-	fmt.Printf("pub=%s\n", pub.SerializeToHexStr())
-	sig := sec.SignHashWithDomain(msg[:])
-	fmt.Printf("sig=%s\n", sig.SerializeToHexStr())
-	fmt.Printf("verify=%v\n", sig.VerifyHashWithDomain(pub, msg[:]))
-}
-
 func sample3() {
+	fmt.Printf("sample3\n")
 	var sec bls.SecretKey
 	b := make([]byte, 64)
 	for i := 0; i < len(b); i++ {
@@ -48,9 +48,24 @@ func sample3() {
 	fmt.Printf("sec=%x\n", sec.Serialize())
 }
 
+func sample4() {
+	fmt.Printf("sample4\n")
+	var sec bls.SecretKey
+	secByte, _ := hex.DecodeString("4aac41b5cb665b93e031faa751944b1f14d77cb17322403cba8df1d6e4541a4d")
+	sec.Deserialize(secByte)
+	msg := []byte("message to be signed.")
+	fmt.Printf("sec:%x\n", sec.Serialize())
+	pub := sec.GetPublicKey()
+	fmt.Printf("pub:%x\n", pub.Serialize())
+	sig := sec.SignByte(msg)
+	fmt.Printf("sig:%x\n", sig.Serialize())
+}
+
 func main() {
 	bls.Init(bls.BLS12_381)
+	bls.SetETHmode(bls.EthModeDraft07)
 	sample1()
 	sample2()
 	sample3()
+	sample4()
 }
