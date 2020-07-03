1139807c8661d325bcd3f54744c71d552ff50b60
MITSUNARI Shigeo <herumi@nifty.com>
2020-07-03 15:51:59 +0900

    remove old tests

--- bls/bls_test.go
+++ bls/bls_test.go
@@ -321,63 +321,6 @@ func ethVerifyTest(t *testing.T) {
 	}
 }
 
-func ethAggregateTestOld(t *testing.T) {
-	msgHexTbl := []string{
-		"b2a0bd8e837fc2a1b28ee5bcf2cddea05f0f341b375e51de9d9ee6d977c2813a5c5583c19d4e7db8d245eebd4e502163076330c988c91493a61b97504d1af85fdc167277a1664d2a43af239f76f176b215e0ee81dc42f1c011dc02d8b0a31e32",
-		"b2deb7c656c86cb18c43dae94b21b107595486438e0b906f3bdb29fa316d0fc3cab1fc04c6ec9879c773849f2564d39317bfa948b4a35fc8509beafd3a2575c25c077ba8bca4df06cb547fe7ca3b107d49794b7132ef3b5493a6ffb2aad2a441",
-		"a1db7274d8981999fee975159998ad1cc6d92cd8f4b559a8d29190dad41dc6c7d17f3be2056046a8bcbf4ff6f66f2a360860fdfaefa91b8eca875d54aca2b74ed7148f9e89e2913210a0d4107f68dbc9e034acfc386039ff99524faf2782de0e"}
-	sigHex := "973ab0d765b734b1cbb2557bcf52392c9c7be3cd21d5bd28572d99f618c65e921f0dd82560cc103feb9f000c23c00e660e1364ed094f137e1045e73116cd75903af446df3c357540a4970ec367a7f7fa7493a5db27ca322c48d57740908585e8"
-	n := len(msgHexTbl)
-	sigVec := make([]Sign, n)
-	for i, sigHex := range msgHexTbl {
-		var sig Sign
-		if sig.DeserializeHexStr(sigHex) != nil {
-			t.Fatalf("bad sig")
-		}
-		sigVec[i] = sig
-	}
-	var aggSig Sign
-	aggSig.Aggregate(sigVec)
-	s := aggSig.SerializeToHexStr()
-	if s != sigHex {
-		t.Fatalf("bad aggregate %v %v\n", s, sigHex)
-	}
-}
-
-func ethAggregateVerifyNoCheckTest(t *testing.T) {
-	pubHexTbl := []string{
-		"a491d1b0ecd9bb917989f0e74f0dea0422eac4a873e5e2644f368dffb9a6e20fd6e10c1b77654d067c0618f6e5a7f79a",
-		"b301803f8b5ac4a1133581fc676dfedc60d891dd5fa99028805e5ea5b08d3491af75d0707adab3b70c6a6a580217bf81",
-		"b53d21a4cfd562c469cc81514d4ce5a6b577d8403d32a394dc265dd190b47fa9f829fdd7963afdf972e5e77854051f6f",
-	}
-	msgHexTbl := []string{
-		"0000000000000000000000000000000000000000000000000000000000000000",
-		"5656565656565656565656565656565656565656565656565656565656565656",
-		"abababababababababababababababababababababababababababababababab",
-	}
-	sigHex := "82f5bfe5550ce639985a46545e61d47c5dd1d5e015c1a82e20673698b8e02cde4f81d3d4801f5747ad8cfd7f96a8fe50171d84b5d1e2549851588a5971d52037218d4260b9e4428971a5c1969c65388873f1c49a4c4d513bdf2bc478048a18a8"
-	n := len(pubHexTbl)
-	var sig Sign
-	if sig.DeserializeHexStr(sigHex) != nil {
-		t.Fatalf("bad sig")
-	}
-	pubVec := make([]PublicKey, n)
-	var msgVec []byte
-	for i := 0; i < n; i++ {
-		if pubVec[i].DeserializeHexStr(pubHexTbl[i]) != nil {
-			t.Fatalf("bad pub")
-		}
-		b, _ := hex.DecodeString(msgHexTbl[i])
-		msgVec = append(msgVec, b...)
-	}
-	if !AreAllMsgDifferent(msgVec) {
-		t.Fatalf("bad msgVec")
-	}
-	if !sig.AggregateVerifyNoCheck(pubVec, msgVec) {
-		t.Fatalf("bad sig")
-	}
-}
-
 func ethFastAggregateVerifyTest(t *testing.T) {
 	fileName := "tests/fast_aggregate_verify.txt"
 	fp, err := os.Open(fileName)
@@ -461,25 +404,6 @@ func blsAggregateVerifyNoCheckTest(t *testing.T) {
 	for i := 0; i < len(nTbl); i++ {
 		blsAggregateVerifyNoCheckTestOne(t, nTbl[i])
 	}
-}
-
-func testEthDraft05(t *testing.T) {
-	if err := SetETHmode(EthModeDraft05); err != nil {
-		t.Fatal(err)
-	}
-	ethAggregateTestOld(t)
-	ethAggregateVerifyNoCheckTest(t)
-	blsAggregateVerifyNoCheckTest(t)
-}
-
-func testEthDraft06(t *testing.T) {
-	if err := SetETHmode(EthModeDraft06); err != nil {
-		t.Fatal(err)
-	}
-	secHex := "0000000000000000000000000000000000000000000000000000000000000001"
-	msgHex := "61736466"
-	sigHex := "8c858cfbec5fed26cdf9368337900a7bec132b4356e959d9e94b8e9178f8669598a46cd12eadf2226d796f6429b527fc067112244c2b15f3b7f6d5f6304c51a7b087664eaabc3c76e745daeafe6930f5699a6a0d4a24486aa886b3770a63ed32"
-	ethSignOneTest(t, secHex, msgHex, sigHex)
 }
 
 func ethAggregateTest(t *testing.T) {
@@ -571,7 +495,7 @@ func ethAggregateVerifyTest(t *testing.T) {
 			continue
 		}
 		out := outHex[1] == "true"
-		b := sig.AggregateVerify(pubVec, msg)
+		b := sig.AggregateVerifyNoCheck(pubVec, msg)
 		if b != out {
 			t.Fatalf("bad AggregateVerify")
 		}
@@ -591,6 +515,7 @@ func testEthDraft07(t *testing.T) {
 	ethVerifyTest(t)
 	ethAggregateTest(t)
 	ethAggregateVerifyTest(t)
+	blsAggregateVerifyNoCheckTest(t)
 }
 
 func Test(t *testing.T) {
@@ -612,8 +537,6 @@ func Test(t *testing.T) {
 	testCompressedG2(t, &gen2)
 	testSignAndVerifyHash(t)
 	testVerifyAggreageteHash(t)
-	testEthDraft05(t)
-	testEthDraft06(t)
 	testEthDraft07(t)
 }
 
