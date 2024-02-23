0e78274cb9ea5cb6aa7134a167cbf77a8f682780
MITSUNARI Shigeo <herumi@nifty.com>
2024-02-23 10:00:38 +0900

    link stdc++ on only x64 cpu

--- bls/link.go
+++ bls/link.go
@@ -4,19 +4,19 @@
 package bls
 
 /*
-#cgo LDFLAGS:-lbls384_256 -lstdc++ -lm
+#cgo LDFLAGS:-lbls384_256 -lm
 #cgo android,arm64 LDFLAGS:-L${SRCDIR}/lib/android/arm64-v8a
 #cgo android,arm LDFLAGS:-L${SRCDIR}/lib/android/armeabi-v7a
-#cgo android,amd64 LDFLAGS:-L${SRCDIR}/lib/android/x86_64
-#cgo linux,amd64 LDFLAGS:-L${SRCDIR}/lib/linux/amd64
+#cgo android,amd64 LDFLAGS:-L${SRCDIR}/lib/android/x86_64 -lstdc++
+#cgo linux,amd64 LDFLAGS:-L${SRCDIR}/lib/linux/amd64 -lstdc++
 #cgo linux,arm64 LDFLAGS:-L${SRCDIR}/lib/linux/arm64
 #cgo linux,mipsle LDFLAGS:-L${SRCDIR}/lib/linux/mipsel
 #cgo linux,arm LDFLAGS:-L${SRCDIR}/lib/linux/arm
 #cgo linux,s390x LDFLAGS:-L${SRCDIR}/lib/linux/s390x
-#cgo darwin,amd64 LDFLAGS:-L${SRCDIR}/lib/darwin/amd64
+#cgo darwin,amd64 LDFLAGS:-L${SRCDIR}/lib/darwin/amd64 -lstdc++
 #cgo darwin,arm64 LDFLAGS:-L${SRCDIR}/lib/darwin/arm64
-#cgo windows,amd64 LDFLAGS:-L${SRCDIR}/lib/windows/amd64
-#cgo openbsd,amd64 LDFLAGS:-L${SRCDIR}/lib/openbsd/amd64
-#cgo freebsd,amd64 LDFLAGS:-L${SRCDIR}/lib/freebsd/amd64
+#cgo windows,amd64 LDFLAGS:-L${SRCDIR}/lib/windows/amd64 -lstdc++
+#cgo openbsd,amd64 LDFLAGS:-L${SRCDIR}/lib/openbsd/amd64 -lstdc++
+#cgo freebsd,amd64 LDFLAGS:-L${SRCDIR}/lib/freebsd/amd64 -lstdc++
 */
 import "C"
