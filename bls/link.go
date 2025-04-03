451a8187115eb617b8f650818ea5757f695d968f
MITSUNARI Shigeo <herumi@nifty.com>
2025-04-03 15:09:00 +0900

    fix conflicts

--- bls/link.go
+++ bls/link.go
@@ -13,6 +13,7 @@ package bls
 #cgo linux,mipsle LDFLAGS:-L${SRCDIR}/lib/linux/mipsel
 #cgo linux,arm LDFLAGS:-L${SRCDIR}/lib/linux/arm
 #cgo linux,s390x LDFLAGS:-L${SRCDIR}/lib/linux/s390x
+#cgo linux,riscv64 LDFLAGS:-L${SRCDIR}/lib/linux/riscv64
 #cgo darwin,amd64 LDFLAGS:-L${SRCDIR}/lib/darwin/amd64
 #cgo darwin,arm64 LDFLAGS:-L${SRCDIR}/lib/darwin/arm64
 #cgo windows,amd64 LDFLAGS:-L${SRCDIR}/lib/windows/amd64 -lstdc++
